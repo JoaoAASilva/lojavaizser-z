706524d058003cdd23f155c17570cc7afdf58e97
JoaoAASilva <araujojoaoarthur31@gmail.com>
2025-06-05 11:37:02 -0300

    inicio  do projeto

---
+++ static/site.cpp
@@ -0,0 +1,12 @@
+meu_projeto/
+├── static/
+│   └── img/
+│       ├── camiseta.jpg
+│       ├── tenis.jpg
+│       ├── calca.jpg
+│       ├── salto.jpg
+│       ├── jaqueta.jpg
+│       └── bota.jpg
+├── templates/
+│   └── catalogo.html
+├── app.py
