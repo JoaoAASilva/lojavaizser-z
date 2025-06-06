02347769dc654ab40c189156cbd435e8607cb393
JoaoAASilva <araujojoaoarthur31@gmail.com>
2025-06-06 10:35:04 -0300

    vcvc

--- views.py
+++ views.py
@@ -1,12 +1,15 @@
 from main import app
 from flask import render_template
-
-# Rotas
-@app.route("/")
+from flask import Flask, render_template
+p.route("/")
 def homepage():
     return render_template("homepage.html")
 
 @app.route("/blog")
 def blog():
-    return "Bem-vindo ao Vaizser Z — seu site de roupas casuais e esportivas."
+    return render_template("blog.html")
 
+@app.route("/catalogo")
+def catalogo():
+    return 
+render_template('catalogo_completo.html')
